ae9ebc96ef2de2d95b1df523818896c898a1fec6
Nauval <jiyanfalih24@gmail.com>
2023-01-12 20:20:51 +0700

    Eduwork

---
+++ Section 6/Tugas E2E_All Process/E2E.cy.js
@@ -0,0 +1,50 @@
+/// <reference types="cypress" />
+
+describe('E2E', () => {
+
+    it('Visit the website', () => {
+        cy.clearCookies()
+        cy.clearLocalStorage()
+        cy.request('https://www.saucedemo.com/')
+        cy.visit('https://www.saucedemo.com/')
+        cy.url().should('include','saucedemo.com')
+
+        // Use Fixtures Data in Login
+        cy.fixture("user").then(user => {
+           const username = user.username
+           const password = user.password
+           const Firstname = user.Firstname
+           const Lastname = user.Lastname
+           const ZipCode = user.ZipCode
+
+        // Input Username
+            cy.get('#user-name').type(username)
+
+        // Input Password
+            cy.get('#password').type(password)
+            cy.get('#login-button').click()
+        
+        // Sort A-Z
+            cy.sortAZ()
+
+        //  Add To Cart
+            cy.AddCart()
+        
+        // Remove Cart
+            cy.RemoveCart()
+        
+        // Add to Cart
+            cy.AddCart()
+        
+        // CheckOut
+            cy.CheckOut()
+        
+        // Input Information
+            cy.InputInfo()
+
+        // Logout
+            cy.Logout()
+
+        });
+    });
+});
---
+++ Section 6/Tugas E2E_All Process/support/commands.js
@@ -0,0 +1,83 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('Login', (username,password) => {
+    cy.clearCookies()
+    cy.clearLocalStorage()
+    cy.get('#user_login').clear()
+    cy.get('#user_login').type('standard_user')
+
+        // Clear and Input Password
+    cy.get('input[name="user_password"]').clear()
+    cy.get('input[name="user_password"]').type('secret_sauce')
+        // Sign in
+    cy.get('#login-button').click()
+})
+
+Cypress.Commands.add('sortAZ', () => {
+    cy.get('.product_sort_container').select('az', 'Name (A to Z)')
+})
+
+Cypress.Commands.add('AddCart', () => {
+    cy.get('.inventory_item_name').should('be.visible')
+    cy.get('#add-to-cart-sauce-labs-backpack').click()
+    cy.get('.inventory_item_name').should('be.visible')
+    cy.get('#add-to-cart-sauce-labs-bike-light').click()
+
+    cy.get('.shopping_cart_link').click()
+})
+
+Cypress.Commands.add('RemoveCart', () => {
+    cy.get('.cart_button').should('be.visible')
+    cy.get('#remove-sauce-labs-bike-light').click()
+    cy.get('.cart_button').should('be.visible')
+    cy.get('#remove-sauce-labs-backpack').click()
+    
+    cy.get('#continue-shopping').click()
+})
+
+Cypress.Commands.add('CheckOut', () => {
+    cy.get('.checkout_button').should('be.visible')
+    cy.get('#checkout').click()
+})
+
+Cypress.Commands.add('InputInfo', () => {
+    cy.get('#first-name').clear()
+    cy.get('#first-name').type('Jiyan Falih')
+    cy.get('#last-name').clear()
+    cy.get('#last-name').type('Nauval')
+    cy.get('#postal-code').clear()
+    cy.get('#postal-code').type('16515')
+
+    cy.get('#continue').click()
+    cy.get('#finish').click()
+    cy.get('#back-to-products').click()
+})
+
+Cypress.Commands.add('Logout', () => {
+    cy.get('#react-burger-menu-btn').click()
+    cy.contains('Logout').click()
+})
